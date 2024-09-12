e40a57c4c3b24835b160021f9038d143125928f5
Khun Malen <malen8622@gmail.com>
2024-09-12 09:18:52 +0700

    initial project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+//Error Handling
+
+//Import the dynamic route loader
+
+const app = express();
+
+// Global Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+if (process.env.DB_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Use the dynamically loaded routes
+
+//Handling Unhandle Routes
+
+
+// Global Error Handling Middleware 
+
+module.app;
